fc666cc549131317fd4b3446382caf887d7e54cf
Curt Henrichs <cdhenrichs@wisc.edu>
2019-10-10 16:48:45 -0500

    major restructure into multi-ros-package repo; worked on updating fake driver

--- taltosoid_driver/src/fake_rik_driver.py
+++ taltosoid_driver/src/fake_rik_driver.py
@@ -5,25 +5,20 @@ TODO Documentation
 '''
 
 import rospy
+
+from core import JOINT_LIST
 from sensor_msgs.msg import JointState
 from relaxed_ik.msg import JointAngles
-
-
-JOINT_LIST = [
-    'finger_root_to_rotation_root',
-    'rotation_root_to_finger_segment_1',
-    'finger_segment_1_to_finger_hinge_1',
-    'finger_segment_2_to_finger_hinge_2'
-]
 
 
 class FakeDriver:
 
     def __init__(self):
         self._js_pub = rospy.Publisher('/taltosoid_driver/joint_state', JointState, queue_size=5)
-        self._js_sub = rospy.Subscriber('/relaxed_ik/joint_angle_solutions', JointAngles, self._joint_update_cb)
+        self._rik_sub = rospy.Subscriber('/relaxed_ik/joint_angle_solutions', JointAngles, self._rik_update_cb)
+        self._js_sub = rospy.Subscriber('/taltosoid_driver/joints_in', JointState, self._js_update_cb)
 
-    def _joint_update_cb(self, msg):
+    def _rik_update_cb(self, msg):
         js = JointState()
         js.header = msg.header
         js.name = JOINT_LIST
@@ -32,10 +27,14 @@ class FakeDriver:
         js.effort = [0] * len(js.position)
         self._js_pub.publish(js)
 
+    def _js_update_cb(self, msg):
+        for j in msg.name:
+            if not j in JOINT_LIST:
+                return # ignore malformed messages
+        self._js_pub.publish(msg)
+
 
 if __name__ == "__main__":
     rospy.init_node('taltosoid_driver')
-
     node = FakeDriver()
-
     rospy.spin()
